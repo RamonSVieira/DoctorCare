304c0daa775c77e4a85b7ac3e942e8891b62bdb5
Ramon Vieira <ramonvieira122333@gmail.com>
2022-05-11 17:21:10 -0300

    desenvolvimento da sessão de contato e rodapé do sito

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,23 @@
+window.addEventListener("scroll", onScroll);
+
 function onScroll() {
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add("scroll");
   } else {
     navigation.classList.remove("scroll");
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 600) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
   }
 }
 
@@ -17,8 +32,7 @@ function closeMenu() {
 ScrollReveal({
   origin: "top",
   distance: "60px",
-  duration: 600,
-  reset: true,
+  duration: 500,
 }).reveal(`
   #introduction,
   #introduction .cont-img img,
